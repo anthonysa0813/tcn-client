e76acdec07e108559e78ce4cd7e5c24568806411
anthony <anthony0813@gmail.com>
2023-04-13 08:44:44 -0500

    nada

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,12 +52,12 @@ const CampaignPage = ({ services }: ServiceProp) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  // const response = await fetch(`${process.env.DB_URL}/services`);
-  // const data = await response.json();
+  const response = await fetch(`${process.env.DB_URL}/services`);
+  const data = await response.json();
   return {
     props: {
-      // services: [data.services],
-      services: [],
+      // services: [],
+      services: [data.services],
     },
   };
 };
